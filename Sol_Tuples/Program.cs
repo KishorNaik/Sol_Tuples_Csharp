46aaf4738ec75f95cb8a3958600d5461171f1100
Kishor Naik <kishor.naik011.net@gmail.com>
2019-06-11 21:15:55 +0530

    Code Commit

---
+++ Sol_Tuples/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Sol_Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            (String firstName,String lastName) tuplesUserObj=(
+                firstName:"Kishor",
+                lastName:"Naik"
+
+            );
+
+            Console.WriteLine(tuplesUserObj.firstName);
+            Console.WriteLine(tuplesUserObj.lastName);
+
+
+            var userCommunicationObj=(emailId : "kishor.naik011.net@gmail.com", mobileNo: "0101010101");
+                Console.WriteLine(userCommunicationObj.emailId);
+                Console.WriteLine(userCommunicationObj.mobileNo);
+
+            
+            // Set Value
+            var tuplesDemoObj=new TuplesDemo();
+                tuplesDemoObj.SetValues((
+
+                    firstName:"Kishor",
+                    lastName:"naik"
+                ));
+
+            // Get Values
+            var tupleUserObj1=tuplesDemoObj.GetValues();
+                Console.WriteLine(tupleUserObj1.firstName);
+                Console.WriteLine(tupleUserObj1.lastName);
+
+
+
+        }
+    }
+
+    public class TuplesDemo{
+
+        // Tuples as Parameter
+        public void SetValues((string firstName,string lastName) tuplesUserObj)
+        {
+            Console.WriteLine(tuplesUserObj.firstName);
+            Console.WriteLine(tuplesUserObj.lastName);
+        }
+
+        // Tuples as Return Type
+        public (String firstName,String lastName) GetValues(){
+
+            var tuplesUserObj=(
+
+                    firstName:"Kishor",
+                    lastName:"Naik"
+            );
+
+            return tuplesUserObj;
+
+        }
+
+    }
+}
